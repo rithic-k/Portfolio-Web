76dabc7519af628f036ec75473ded23b2e3dbf48
Rithic <rithickrishna.k@gmail.com>
2025-04-19 07:20:36 +0000

    fixed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,37 +78,90 @@ export default function Home() {
   }, []);
 
     const certificationsData = [
-    {
-      name: "AWS Certified Cloud Practitioner",
-      issuingOrganization: "Amazon Web Services (AWS)",
-    },
-    {
+       {
       name: "Microsoft Certified: Azure Fundamentals",
       issuingOrganization: "Microsoft",
+      credentialUrl: "https://www.credly.com/badges/6e1a5df7-d937-4e91-a20a-95494cd623ff/public_url"
     },
     {
       name: "Google Cloud Certified - Cloud Digital Leader",
       issuingOrganization: "Google Cloud",
+      credentialUrl: "https://www.credential.net/9e6ef039-0f9c-4917-9287-c446a18e509e"
     },
-        {
+    {
+      name: "AWS Certified Cloud Practitioner",
+      issuingOrganization: "Amazon Web Services (AWS)",
+      credentialUrl: "https://www.credly.com/badges/9e492459-c1c1-49f2-b548-b8ac86250012/public_url"
+    },
+   {
       name: "Architecting with Google Compute Engine",
       issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/763G8XG486HR"
     },
         {
       name: "Networking in Google Cloud Specialization",
       issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/certificate/YYG2EADTZR9H"
     },
             {
       name: "Security in Google Cloud Specialization",
       issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/certificate/HSV9D55XCCAN"
     },
     {
       name: "Data Engineering on Google Cloud Platform Specialization",
       issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/certificate/5QKF4FTCZ7HA"
     },
     {
       name: "Preparing for Google Cloud Certification: Cloud Architect Professional Certificate",
       issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/QWERTYUIOP"
+    },
+        {
+      name: "Google Cloud Fundamentals: Core Infrastructure",
+      issuingOrganization: "Coursera",
+      credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/R2LQG2G82F2S"
+    },
+            {
+      name: "Elastic Kubernetes Service (EKS) with Terraform",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-ec14e7d2-1f56-447a-9cae-b01c99906a4e/"
+    },
+            {
+      name: "AWS Certified Solutions Architect – Associate SAA-C03",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-6c285737-8c41-472f-a491-4b04e6ee1f29/"
+    },
+        {
+      name: "Terraform on AWS",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-d7614c4e-4c19-4a8e-bb4d-f2efef952308/"
+    },
+        {
+      name: "Amazon VPC MasterClass",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-f3ac1164-b905-459f-9f72-c8b860b1419f/"
+    },
+         {
+      name: "Docker on AWS",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-468459e3-9960-4587-8847-f7ff95c7899a/"
+    },
+             {
+      name: "AWS Certified Developer – Associate DVA-C02",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-cc9849b1-9943-4273-a703-94519c292a5c/"
+    },
+    {
+      name: "AWS Certified SysOps Administrator – Associate SOA-C02",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-415c196f-275a-4543-84a9-959e66e6c9c2/"
+    },
+     {
+      name: "AWS Certified DevOps Engineer – Professional DOP-C01",
+      issuingOrganization: "Udemy",
+      credentialUrl: "https://www.udemy.com/certificate/UC-79528dd2-ea92-4322-a941-52f3c8ee7769/"
     },
   ];
 
@@ -176,13 +229,15 @@ export default function Home() {
         {/* Certifications Section */}
         <section id="certifications" ref={sectionRefs.certifications} className="mb-16">
           <h2 className="text-3xl font-semibold mb-4 uppercase">Certifications</h2>
-             <ul className="list-disc list-inside">
-          {certificationsData.map((cert, index) => (
-            <li key={index}>
-              {cert.name} - {cert.issuingOrganization}
-            </li>
-          ))}
-        </ul>
+            <ul className="list-disc list-inside">
+              {certificationsData.map((cert, index) => (
+                 <li key={index}>
+                    <a href={cert.credentialUrl} target="_blank" rel="noopener noreferrer">
+                       {cert.name} - {cert.issuingOrganization}
+                    </a>
+                 </li>
+              ))}
+           </ul>
         </section>
 
         {/* Contact Section */}
