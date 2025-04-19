28820e2d0404679c37938fcab1208930bbe8b2b8
Rithic <rithickrishna.k@gmail.com>
2025-04-19 07:41:33 +0000

    add moving curvy geometrical lines as background random curvy mivements not uniform

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
+import CanvasBackground from './components/CanvasBackground';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="geometric-background"></div>
+        <CanvasBackground />
         {children}
         </body>
     </html>
